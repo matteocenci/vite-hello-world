f504241eb6732dc88ff6d5ce9623670bca9e7fa9
matteocenci <matteocenci01@gmail.com>
2024-04-30 17:40:23 +0200

    feat: init project exercise done

---
+++ src/components/AppTitle.vue
@@ -0,0 +1,19 @@
+<script>
+export default {
+  
+  data () {
+    return{
+      title: "this is my title"
+
+    };
+  },
+};
+</script>
+
+<template>
+  <h1>{{title}}</h1>
+</template>
+
+<style>
+</style>
+
